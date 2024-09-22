a8ab924d8b151904f6d87924e9f7a26e1438570a
Ricardo <ricardo.elera.cumpa@gmail.com>
2024-09-22 09:49:18 -0500

    Actualización del repositorio

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const commission = document.getElementById('commission').value;
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
 
-    if (!name || !email || !commission) {
-        alert("Por favor, completa todos los campos.");
-        event.preventDefault(); // Evita el envío del formulario
+    if (form) {
+        form.addEventListener('submit', function(event) {
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const commission = document.getElementById('commission').value;
+
+            if (!name || !email || !commission) {
+                alert("Por favor, completa todos los campos.");
+                event.preventDefault(); // Evita el envío del formulario
+            }
+        });
+    } else {
+        console.warn('El formulario no se encontró');
     }
 });
